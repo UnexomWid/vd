2ac3f04f6aaf7c2b8cddb128c2a1584e5b236f16
UnexomWid <uw@exom.dev>
2022-06-01 20:26:16 +0300

    modify: naninovel detector

--- src/detectors/naninovel.py
+++ src/detectors/naninovel.py
@@ -5,5 +5,5 @@ import glob
 
 
 def detect(path):
-    if any(os.path.isfile(dll) for dll in glob.iglob(os.path.join(path, '**/Naninovel*.dll'), recursive=True)):
+    if any(os.path.isfile(dll) for dll in glob.iglob(os.path.join(path, '**/*Naninovel*.dll'), recursive=True)):
         return 'Naninovel'
